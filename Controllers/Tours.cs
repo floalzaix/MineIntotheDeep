624618041b0c4d607e2f7de82a8ec2896a12545c
floalzaix <floalzfencing@gmail.com>
2025-04-07 13:57:02 +0200

    Added the controller Tours

---
+++ Controllers/Tours.cs
@@ -0,0 +1,97 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Timers;
+using MineIntoTheDeep.models;
+using MineIntoTheDeep.Models;
+
+namespace MineIntoTheDeep.Controllers
+{
+    public class Tours
+    {
+        // Class variables
+        public static readonly int NB_MAX_ACTIONS_PER_TURN = 2;
+        public static readonly int TIME_PER_TURN = 60 * 1000;
+
+        // Instance variables
+        public Carte Carte { get; init; }
+        public Joueur[] Joueurs { get; init; }
+        public int Index { get; set; }
+        public Queue<int> Turns { get; set; }
+        public int[] CurrentTurns { get; set; }
+
+        private readonly Timer timer;
+
+        // Constructors
+        public Tours(Carte carte, Joueur[] joueurs, int index = 0)
+        {
+            Carte = carte;
+            Joueurs = joueurs;
+            Index = index;
+            Turns = new Queue<int>(Enumerable.Range(0, Joueurs.Length));
+            CurrentTurns = [.. Turns];
+
+            Joueurs[CurrentTurns[Index]].Actions = NB_MAX_ACTIONS_PER_TURN;
+
+            // Timer
+            timer = new Timer(TIME_PER_TURN);
+            timer.Elapsed += OnTimer;
+        }
+
+        //
+        //  Functions
+        //
+
+        /// <summary>
+        /// Switch to the next player by putting the precedent player actions to none
+        /// and giving the current player the actions to play.
+        /// </summary>
+        public void Next()
+        {
+            bool en = timer.Enabled;
+            if (en)
+            {
+                timer.Stop();
+            }
+            Joueurs[CurrentTurns[Index]].Actions = 0;
+            Index++;
+            if (Index >= Turns.Count)
+            {
+                Index = 0;
+                Turns.Enqueue(Turns.Dequeue());
+                CurrentTurns = [.. Turns];
+            }
+            Joueurs[CurrentTurns[Index]].Actions = NB_MAX_ACTIONS_PER_TURN;
+            if (en)
+            {
+                timer.Start();
+            }
+        }
+
+        public void OnTimer(object? src, ElapsedEventArgs e)
+        {
+            Next();
+        }
+
+        public void Start()
+        {
+            timer.Enabled = true;
+            timer.Start();
+        }
+
+        public void Stop()
+        {
+            Joueurs[CurrentTurns[Index]].Actions = 0;
+            timer.Stop();
+            timer.Dispose();
+        }
+
+        //
+        //  Overrides
+        //
+
+        public override string ToString()
+        {
+            return $"Players whos turn it is : {Joueurs[CurrentTurns[Index]]}";
+        }
+    }
+}
--- obj/Debug/net8.0/MineIntoTheDeep.AssemblyInfo.cs
+++ obj/Debug/net8.0/MineIntoTheDeep.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MineIntoTheDeep")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0da9718fb4ac6eaf56c323f9c36781fccc003864")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4be6579e21b7eae42f0fa87ba4ed525e08a7ad17")]
 [assembly: System.Reflection.AssemblyProductAttribute("MineIntoTheDeep")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MineIntoTheDeep")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
